24ac92e73d23313588b637b07e7654726187330e
lukaszcichon48 <lukasz.cichon@sealights.io>
2022-05-19 14:11:15 +0200

    Added shippingservice

---
+++ push.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+
+docker build -t microservices-demo-shippingservice .
+docker tag microservices-demo-shippingservice:latest 159616352881.dkr.ecr.eu-west-1.amazonaws.com/microservices-demo-shippingservice:latest
+docker push 159616352881.dkr.ecr.eu-west-1.amazonaws.com/microservices-demo-shippingservice:latest
